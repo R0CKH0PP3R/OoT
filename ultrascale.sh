a9aefb696fe16717612472d80f4d78ab63469b39
R0CKH0PP3R <49409328+R0CKH0PP3R@users.noreply.github.com>
2023-08-03 22:58:57 +0100

    V0.2 - practically seamless
    This consolidates my other efforts. The montage is repeated for one tile at each side and a 2px mirror applied to the bottom. An additional 2 px are also copied from the next image when slicing. This requires additional post-processing but virtually eliminates visible seams.

--- ultrascale.sh
+++ ultrascale.sh
@@ -6,7 +6,11 @@
 # The panorama is then sliced up into constituent parts and padding reapplied. 
 # Scaling this way gives the model more context and avoids visible seams in game.
 
-# Check dependancies. You can get a portable release of realesrgan from GitHub:
+# V0.2 - The montage is repeated for one tile at each side and a 2px mirror applied
+# to the bottom. An additional 2 px are also copied from the next image when slicing.
+# This requires additional post-processing but virtually eliminates visible seams.
+
+# Check dependencies. You can get a portable release of realesrgan from GitHub:
 # https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan
 type mogrify >/dev/null || { echo "Please install ImageMagick to use this script."; exit 1; }
 type realesrgan-ncnn-vulkan >/dev/null || { echo "realesrgan-ncnn-vulkan not in PATH"; exit 1; }
@@ -15,10 +19,10 @@ type realesrgan-ncnn-vulkan >/dev/null || { echo "realesrgan-ncnn-vulkan not in 
 [[ ! -d "$1" ]]  && echo "No directory provided." && exit 1
 [[ "$#" -ne 1 ]] && echo "Please, one at a time." && exit 1
 
-# Create temporary dirs & vars. I make an effort to clean these up as we go.
+# Create tmp dirs & vars. I make an effort to clean these up as we go.
 tmpdir='/tmp/ncnn'
-scaled="$tmpdir"/scaled
-mkdir -p "$scaled"
+scaled="${tmpdir}/scaled"
+mkdir -p $scaled 
 
 ncnn(){
 	# With realesrgan in PATH, we just need to provide an image ($2) and a model ($1).
@@ -32,7 +36,7 @@ ncnn(){
 	realesrgan-ncnn-vulkan -i "$file" -o "$file" -n "$1" 2>&1 | tee "$tmpdir"/.log
 	# And because it ploughs through errors and provides no exit status...	
 	failed=$(cat "$tmpdir"/.log | grep failed)
-	[[ -n $failed ]] && ncnn "$1" "$2" || { cp "$file" "$2" && rm "$file"; }
+	[[ -n $failed ]] && ncnn "$1" "$2" || mv "$file" "$2"
 }
 
 msg(){
@@ -40,48 +44,101 @@ msg(){
 	rm -r "$tmpdir"
 	[[ $1 -eq 0 ]] && echo "All done." && exit 0
 	[[ $1 -eq 1 ]] && echo "Montage Failed. Process halted." && exit 1
+	[[ $1 -eq 2 ]] && echo "Slicing Failed. Process halted." && exit 1
+}
+
+magick(){
+	# This is where it happens. :)
+	local -n panarr="$1"
+	
+	# Remove the margins/padding 
+	for png in "${panarr[@]}"; do mogrify -crop 253x249+0+0 "$png"; done
+	
+	# Create panoramic montage for seamless upscale.
+	montage "${panarr[@]}" -tile "${#panarr[@]}x1" -geometry +0+0 "$tmpdir"/pan.png
+	
+	# Check prior success & upscale with chosen model.
+	[[ $? -eq 0 ]] && ncnn 'ultrasharp' "$tmpdir"/pan.png || msg 1
+	
+	# Determine the size of each slice. Offset is used to keep track of where we are.
+	panw=$(identify -format %w "$tmpdir"/pan.png)
+	width=$((panw/${#panarr[@]}))
+	height=$(identify -format %h "$tmpdir"/pan.png)
+	offset=0
+	
+	# Mirror the panorama along the bottom edge & crop as to only include an extra 2px.
+	convert ${tmpdir}/pan.png -background transparent -extent ${panw}x$((height*2)) \
+			\( +clone -flip \) -composite -crop ${panw}x$((height+2))+0+0 ${tmpdir}/pan.png
+	
+	if (( ${#panarr[@]} == 6 )); then
+		# Unset the first and last idx so that we're not working with either (duplicate) side.
+		unset panarr[0] && unset panarr[-1]
+		offset=$((offset+width))
+		# Take a slice for each image & overwrite the original		
+		for png in "${panarr[@]}"
+		do
+			# Rather than mirror the right edge, take an extra 2px from the next image.
+			# Finally, scale down to proper size, i.e -2px in each dimension & add padding.
+			# Note that the sinc filter is very sharp, Lanczos may be preferred.
+			convert ${tmpdir}/pan.png -filter sinc -crop $((width+2))x$((height+2))+${offset}+0 \
+					-resize ${width}x${height}! -background black -extent 1024x1024 "$png"
+			
+			# Check that it worked. If so, increment the offset and continue.
+			[[ $? -eq 0 ]] && offset=$((offset+width)) || msg 2
+		done
+		
+	else
+		# Now to deal with the panoramas which are not a full 360deg.
+		for png in "${panarr[@]}"
+		do
+			if [[ "$png" == "${panarr[-1]}" ]]; then
+				# This is the last, right-most tile, so there's nothing extra to crop.
+				# Mirror along the right edge & crop as to include an extra 2px and maintain aspect.
+				# Then scale down to pre-mirrored size, i.e -2px in each dimension & add padding.
+				convert ${tmpdir}/pan.png -crop ${width}x$((height+2))+${offset}+0 +repage - | 
+				convert - -filter sinc -background transparent -extent $((width*2))x$((height+2)) \
+						\( +clone -flop \) -composite -crop $((width+2))x$((height+2))+0+0 \
+						-resize ${width}x${height}! -background black -extent 1024x1024 "$png"
+			else
+				# For any other tile, we can take a bit of the next to make it seamless.
+				convert ${tmpdir}/pan.png -filter sinc -crop $((width+2))x$((height+2))+${offset}+0 \
+					-resize ${width}x${height}! -background black -extent 1024x1024 "$png"
+			fi
+			# Check that it worked. If so, increment the offset and continue.
+			[[ $? -eq 0 ]] && offset=$((offset+width)) || msg 2
+		done
+	fi
+
+	rm ${tmpdir}/pan.png
 }
 
 # Recursive list of directories. There has to be at least one.
 readarray -d '' dirs < <(find "$1" -type d -print0)
+
 # Enter each directory in turn.
 for dir in "${dirs[@]}"
 do
 	# Generate a list of files to process within this directory.
+	# Note that numbered hits return first. So 'texture2' comes before 'texture'.
 	readarray -d '' fnd < <(find "$dir" -maxdepth 1 -name '*.png' -print0 | sort -z)
-
-	# Some directories may not have any pngs. Only proceed if they do.
-	if (( ${#fnd[@]} )); then
-		# As the first png wont be numbered, it will be found last. To put them in order, 
-		# create a new array starting with the last png before removing it from the first array.
-		pngs=("${fnd[-1]}") && unset fnd[-1]
-		# And add the remaining pngs.
-		for f in "${fnd[@]}"; do pngs+=("$f"); done
-		
-		# Remove the margins/padding 
-		for png in "${pngs[@]}"; do mogrify -crop 253x249+0+0 "$png"; done
-		# Create panoramic montage for seamless upscale.
-		montage "${pngs[@]}" -tile "${#pngs[@]}"x1 -geometry +0+0 "$tmpdir"/pan.png
-		
-		# Check prior success & upscale with chosen model.
-		[[ $? -eq 0 ]] && ncnn 'ultrasharp' "$tmpdir"/pan.png || msg 1
-
-		# Determine the size of each slice. Offset is used to keep track of where we are.
-		panw=$(identify -format %w "$tmpdir"/pan.png)
-		width=$((panw/${#pngs[@]}))
-		height=$(identify -format %h "$tmpdir"/pan.png)
-		offset=0
-		# Take a slice for each image & overwrite original		
-		for png in "${pngs[@]}"
-		do
-			# create new pngs
-			convert "$tmpdir"/pan.png -crop "$width"x"$height"+"$offset"+0 \
-					-background Black -extent 1024x1024 "$png"
-			offset=$((offset+width))
-		done
+	
+	# 360 panoramas comprise 4 images. To remove all seams, we have to duplicate 2 images.
+	if [ ${#fnd[@]} -eq 4 ]; then 
+		# This results in 'T4 T T2 T3 T4 T' which allows us to make the inner 4 seamless.
+		# Start with the last 2. Note that the syntax here is just 'from idx 2'.
+		sorted=("${fnd[@]:2}")
+		for f in "${fnd[@]}"; do sorted+=("$f"); done
+		magick sorted
+	# Panoramas comprising fewer images don't wrap, so no duplicates required. Just sort them.
+	elif [ ${#fnd[@]} -gt 0 ]; then 
+		# Start with the last one (i.e. the un-numbered 1st texture) 
+		sorted=("${fnd[-1]}") && unset fnd[-1]
+		# And add the remaining ones.
+		for f in "${fnd[@]}"; do sorted+=("$f"); done
+		magick sorted
 	fi
 	
-	# Jpgs are much easier to process as there is only one jpg per scene. 
+	# JPGs are much easier to process as there is only one jpg per scene. 
 	# Therefore, seams are irrelevant and we can go straight to scaling.
 	readarray -d '' jpgs < <(find "$dir" -maxdepth 1 -name '*.jpg' -print0)
 	[[ ${#jpgs[@]} -gt 0 ]] && for jpg in "${jpgs[@]}"; do ncnn 'ultrasharp' "$jpg"; done 
